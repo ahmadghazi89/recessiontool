19316cabb458a9d3eee189bc55d49f9616225eaa
ahmadghazi <ahmadghazi89@gmail.com>
2019-06-05 01:12:29 -0400

    initial upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from splinter import Browser
+from bs4 import BeautifulSoup
+import time
+from selenium import webdriver
+from flask import Flask, jsonify, render_template, request, redirect
+import json
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    """Return the homepage."""
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    browser = Browser('chrome', **executable_path, headless=True)
+
+    url = 'https://www.leggmason.com/global/campaigns/clearbridge-aor-recession-indicator-tool.html'
+    browser.visit(url)
+
+    html = browser.html
+    soup = BeautifulSoup(html, "html.parser")
+
+    value = soup.find("span", id="gauge-value").get_text()
+    return render_template("index.html",percent=int(value)/100, percent2=int(value))
+
+if __name__ == "__main__":
+    app.run()
+
